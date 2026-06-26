ad690db79ad54c9e00001d2a0edfa7292722a063
Ayush Soni <ayushsoni.60226@gmail.com>
2026-06-27 01:42:25 +0530

    Implement JWT login authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -33,6 +33,52 @@ const registerUser = async (req, res) => {
     }
 };
 
+const jwt = require("jsonwebtoken");
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d",
+            }
+        );
+
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
 module.exports = {
     registerUser,
+    loginUser,
 };
