0481127b1eddaf56ec170c0a1e6c6c0972e9e867
Ayush Soni <ayushsoni.60226@gmail.com>
2026-06-26 09:26:57 +0530

    Connected Express backend to MongoDB Atlas using Mongoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.use(cors());
 
